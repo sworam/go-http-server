27fdd088e771a860f180b15d4ea7e2746df70a2f
Marlo Swora <swora@uni-hildesheim.de>
2025-10-14 18:42:21 +0200

    add handlers for metrics

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
 
 func main() {
 	const port = "8080"
 
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	fileHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	apiCfg := apiConfig{
+		fileServerHits: atomic.Int32{},
+	}
+	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(fileHandler))
 	serveMux.HandleFunc("/healthz", handleReadiness)
+	serveMux.HandleFunc("/metrics", apiCfg.handleMetrics)
+	serveMux.HandleFunc("/reset", apiCfg.handleReset)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -25,4 +37,23 @@ func handleReadiness(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	writer.WriteHeader(200)
 	writer.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleMetrics(writer http.ResponseWriter, request *http.Request) {
+	msg := fmt.Sprintf("Hits: %d\n", cfg.fileServerHits.Load())
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	writer.Write([]byte(msg))
+}
+
+func (cfg *apiConfig) handleReset(writer http.ResponseWriter, request *http.Request) {
+	cfg.fileServerHits.Store(0)
+	writer.WriteHeader(200)
 }
