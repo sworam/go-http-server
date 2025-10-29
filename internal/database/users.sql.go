56c69292596f869c9de2c5923f49d77cf7ff0178
marlo <sworam@uni-hildesheim.de>
2025-10-29 19:01:52 +0100

    add hashed_password to user and way to retrieve user with email

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -10,24 +10,49 @@ import (
 )
 
 const createUser = `-- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, email)
+INSERT INTO users (id, created_at, updated_at, email, hashed_password)
 VALUES (
-		gen_random_uuid(),
-		NOW(),
-		NOW(),
-		$1
+	gen_random_uuid(),
+	NOW(),
+	NOW(),
+	$1,
+	$2
 )
-Returning id, created_at, updated_at, email
+Returning id, created_at, updated_at, email, hashed_password
 `
 
-func (q *Queries) CreateUser(ctx context.Context, email string) (User, error) {
-	row := q.db.QueryRowContext(ctx, createUser, email)
+type CreateUserParams struct {
+	Email          string
+	HashedPassword string
+}
+
+func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, error) {
+	row := q.db.QueryRowContext(ctx, createUser, arg.Email, arg.HashedPassword)
 	var i User
 	err := row.Scan(
 		&i.ID,
 		&i.CreatedAt,
 		&i.UpdatedAt,
 		&i.Email,
+		&i.HashedPassword,
+	)
+	return i, err
+}
+
+const getUser = `-- name: GetUser :one
+SELECT id, created_at, updated_at, email, hashed_password FROM users
+WHERE email = $1
+`
+
+func (q *Queries) GetUser(ctx context.Context, email string) (User, error) {
+	row := q.db.QueryRowContext(ctx, getUser, email)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Email,
+		&i.HashedPassword,
 	)
 	return i, err
 }
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,9 +1,14 @@
 -- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, email)
+INSERT INTO users (id, created_at, updated_at, email, hashed_password)
 VALUES (
-		gen_random_uuid(),
-		NOW(),
-		NOW(),
-		$1
+	gen_random_uuid(),
+	NOW(),
+	NOW(),
+	$1,
+	$2
 )
 Returning *;
+
+-- name: GetUser :one
+SELECT * FROM users
+WHERE email = $1;
